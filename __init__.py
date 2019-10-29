04a42d87fe86d3a53db43a989da97001f547202a
Leo Coulson <lcoulson@bloomberglaw.com>
2019-10-29 11:20:42 -0400

    things

--- __init__.py
+++ __init__.py
@@ -1,2 +1,14 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+from crhelper import CfnResource
+import CFrontClasses
+from str2bool import str2bool
+from collections import Counter
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional, List
+import uuid
 from .CFrontClasses import *  # noqa
 from .CfnCustomCFrontDistro import *  # noqa
+
--- setup.py
+++ setup.py
@@ -18,6 +18,13 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+	install_requires=[
+      'boto3>=1.9.189',
+      'botocore>=1.12.189',
+      'crhelper>=2.0.4',
+      'pytest>=5.0.1',
+      'str2bool>=1.1'
+      ],
     python_requires='>=3.7',
 	py_modules=['CfnCustomCFrontDistro','CFrontClasses']
 )
